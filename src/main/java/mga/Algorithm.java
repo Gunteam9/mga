fb36124ccc801203801298ed98009217bbdf4db3
Gunteam9 <58592342+Gunteam9@users.noreply.github.com>
2020-11-12 16:30:00 +0100

    Etape 1

--- src/main/java/mga/Algorithm.java
+++ src/main/java/mga/Algorithm.java
@@ -1,5 +1,129 @@
 package mga;
 
-public class Algorithm {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Random;
 
+public class Algorithm {
+	
+	private final Random rand = new Random();
+	private final ArrayList<String> colors = new ArrayList<String>(Arrays.asList("Bleu", "Rouge", "Vert", "Jaune", "Violet"));
+		
+	public HashMap<Integer, String> coloringRec(HashMap<Integer, ArrayList<Integer>> graph) throws Exception {	
+		HashMap<Integer, String> coloredGraph = null;
+	
+		//On effectue les vérifications
+		//Si le graphe n'est pas vide
+		if (!graph.isEmpty()) {
+
+			//S'il existe un sommet avec un degré supérieur à 5
+			for (Map.Entry<Integer, ArrayList<Integer>> entry : graph.entrySet()) {
+				if (entry.getValue().size() > 5)
+					throw new Exception();
+			}
+			
+			int x = getRandomSommet(graph);
+			HashMap<Integer, ArrayList<Integer>> graphWithoutX = graph;
+			graphWithoutX.remove(x);
+			coloredGraph = coloringRec(graphWithoutX);
+			
+			System.out.println(x);
+			//Brique 4
+			if (graph.get(x).size() <= 4) {
+				for (int vertex : graph.get(x)) {
+					for (String color : colors) {
+						if (!coloredGraph.get(vertex).contains(color)) {
+							coloredGraph.put(x, color);
+							break;
+						}
+					}
+				}	
+			}
+			
+			if (graph.get(x).size() == 5) {
+				ArrayList<String> usedColor = new ArrayList<String>();
+				for (int neighbor : graph.get(x)) {
+					usedColor.add(coloredGraph.get(neighbor));
+				}
+				
+				//Brique 5
+				if (usedColor.size() != 5) {
+					if (isAllElementDifferent(usedColor))
+						coloredGraph.put(x, colors.get(colors.size() - 1));
+				}
+				//Brique 6
+				else {	
+					coloredGraph = brique6(graph, coloredGraph, x);
+				}
+			}
+		}
+		
+		return coloredGraph;
+	}
+	
+	private HashMap<Integer, String> brique6(HashMap<Integer, ArrayList<Integer>> graph, HashMap<Integer, String> coloredGraph, int x) {
+		PriorityQueue<Integer> voisinsAtteint = new PriorityQueue<>();
+		ArrayList<Integer> voisinsConnus = new ArrayList<Integer>();
+		
+		for (Integer voisinDeX : graph.get(x)) {
+			for (Integer voisinDeVoisinDeX : graph.get(voisinDeX)) {
+				if (!voisinsConnus.contains(voisinDeVoisinDeX)) {
+					voisinsConnus.add(voisinDeVoisinDeX);
+					voisinsAtteint.add(voisinDeVoisinDeX);
+				}
+			}
+			
+			while(!voisinsAtteint.isEmpty()) {
+				int element = voisinsAtteint.poll();
+				for (Integer obj : graph.get(element)) {
+					if (!voisinsConnus.contains(obj)) {
+						voisinsConnus.add(obj);
+						voisinsAtteint.add(obj);
+					}
+				}
+			}
+			
+			for (Integer encoreLesVoisinsDeX : graph.get(x)) {
+				if (!voisinsConnus.contains(encoreLesVoisinsDeX)) {
+					coloredGraph = echangerCouleur(graph, coloredGraph, x, encoreLesVoisinsDeX);
+					coloredGraph = echangerCouleur(graph, coloredGraph, encoreLesVoisinsDeX, voisinDeX);
+					coloredGraph = brique6(graph, coloredGraph, x);
+					break;
+				}
+			}
+		}
+		
+		return coloredGraph;
+	}
+
+	private HashMap<Integer, String> echangerCouleur(HashMap<Integer, ArrayList<Integer>> graph, HashMap<Integer, String> coloredGraph, int sommet1, int sommet2) {
+		String color1 = coloredGraph.get(sommet1);
+		
+		coloredGraph.replace(sommet1, coloredGraph.get(sommet2));
+		coloredGraph.replace(sommet2, color1);
+		
+		return coloredGraph;
+		
+	}
+
+	private int getRandomSommet(HashMap<Integer, ArrayList<Integer>> graph) {
+		int graphLength = graph.keySet().size();
+		int randomNumber = rand.nextInt(graphLength);
+				
+		return (Integer) graph.keySet().toArray()[randomNumber];
+	}
+	
+	private <T> boolean isAllElementDifferent(ArrayList<T> array) {
+		for (T element : array) {
+			ArrayList<T> arrayWihoutElement = array;
+			arrayWihoutElement.remove(element);
+			if (arrayWihoutElement.contains(element))
+				return false;
+		}
+		
+		return true;
+	}
 }
--- src/main/java/mga/Launcher.java
+++ src/main/java/mga/Launcher.java
@@ -1,9 +1,34 @@
 package mga;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Launcher {
 
-	public static void main(String[] args) {
-		new Algorithm();
+	public static void main(String[] args) {	
+		HashMap<Integer, ArrayList<Integer>> graph = new HashMap<Integer, ArrayList<Integer>>();
+		HashMap<Integer, String> coloredGraph = null;
+		
+		graph.put(0, new ArrayList<Integer>(Arrays.asList(3)));
+		graph.put(1, new ArrayList<Integer>(Arrays.asList(1,2,3)));
+		graph.put(2, new ArrayList<Integer>(Arrays.asList(1,3)));
+		graph.put(3, new ArrayList<Integer>(Arrays.asList(0,1,2)));
+				
+		Algorithm a = new Algorithm();
+		
+		try {
+			coloredGraph = a.coloringRec(graph);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(coloredGraph);
+		
+		for (Map.Entry<Integer, String> entry : coloredGraph.entrySet()) {
+			System.out.println(entry.getKey() + " est " + entry.getValue());
+		}
 	}
 
 }
